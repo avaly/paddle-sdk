10c57207db3b21c2ce31cc285c015dc332cda60d
Jonne <jonne.huotari@perfektio.fi>
2022-08-16 09:36:04 +0300

    test: Checkout server URL

--- __tests__/attributes.test.js
+++ __tests__/attributes.test.js
@@ -30,6 +30,28 @@ describe('attributes', () => {
 		);
 	});
 
+	test('with checkout API server URL', () => {
+		const instance = new PaddleSDK('foo', 'bar', 'ham');
+
+		expect(instance.vendorID).toBe('foo');
+		expect(instance.apiKey).toBe('bar');
+		expect(instance.publicKey).toBe('ham');
+		expect(instance._serverURL(true)).toBe(
+			'https://checkout.paddle.com/api/1.0'
+		);
+	});
+
+	test('with checkout API sandbox server URL', () => {
+		const instance = new PaddleSDK('foo', 'bar', 'ham', { sandbox: true });
+
+		expect(instance.vendorID).toBe('foo');
+		expect(instance.apiKey).toBe('bar');
+		expect(instance.publicKey).toBe('ham');
+		expect(instance._serverURL(true)).toBe(
+			'https://sandbox-checkout.paddle.com/api/1.0'
+		);
+	});
+
 	test('with custom server URL', () => {
 		const instance = new PaddleSDK('foo', 'bar', 'ham', {
 			server: 'https://custom.paddle.net',
